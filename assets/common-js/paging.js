3c738067555eecf318e698520074cfdc38d7ffc6
Kelv-mutiso <38732483+MutisoKelv@users.noreply.github.com>
2018-11-16 15:33:29 +0300

    version 3.0 -- which contains mass changes

--- assets/common-js/paging.js
+++ assets/common-js/paging.js
@@ -1,28 +1,28 @@
-$("#no1").on('click', function() {
-    $("#pg1").fadeIn();
-    $("#pg2,#pg3,#pg4,#pg5,#pg6,#pg7").fadeOut();
+$("#asked").on('click', function() {
+    $("#ask").fadeIn();
+    $("#ans").fadeOut();
 });
-$("#no2").on('click', function() {
-    $("#pg2").fadeIn();
-    $("#pg1,#pg3,#pg4,#pg5,#pg6,#pg7").fadeOut();
+$("#answered").on('click', function() {
+    $("#ans").fadeIn();
+    $("#ask").fadeOut();
 });
-$("#no3").on('click', function() {
-    $("#pg3").fadeIn();
-    $("#pg2,#pg1,#pg4,#pg5,#pg6,#pg7").fadeOut();
-});
-$("#no4").on('click', function() {
-    $("#pg4").fadeIn();
-    $("#pg2,#pg3,#pg1,#pg5,#pg6,#pg7").fadeOut();
-});
-$("#no5").on('click', function() {
-    $("#pg5").fadeIn();
-    $("#pg2,#pg3,#pg4,#pg1,#pg6,#pg7").fadeOut();
-});
-$("#no6").on('click', function() {
-    $("#pg6").fadeIn();
-    $("#pg2,#pg3,#pg4,#pg5,#pg1,#pg7").fadeOut();
-});
-$("#no7").on('click', function() {
-    $("#pg7").fadeIn();
-    $("#pg2,#pg3,#pg4,#pg5,#pg6,#pg1").fadeOut();
-});
+// $("#qstn").on('click', function() {
+//     $("#questions").fadeIn();
+//     $("#research_notes,#ans").fadeOut();
+// });
+// $("#notes").on('click', function() {
+//     $("#research_notes").fadeIn();
+//     $("#questions").fadeOut();
+// });
+// $("#no5").on('click', function() {
+//     $("#pg5").fadeIn();
+//     $("#pg2,#pg3,#pg4,#pg1,#pg6,#pg7").fadeOut();
+// });
+// $("#no6").on('click', function() {
+//     $("#pg6").fadeIn();
+//     $("#pg2,#pg3,#pg4,#pg5,#pg1,#pg7").fadeOut();
+// });
+// $("#no7").on('click', function() {
+//     $("#pg7").fadeIn();
+//     $("#pg2,#pg3,#pg4,#pg5,#pg6,#pg1").fadeOut();
+// });
